a8088ef58824dafd60d896ee80f1154c56418125
dario <0511dario@gmail.com>
2022-12-31 15:51:50 -0600

    Add Verision

---
+++ Assets/Scripts/Test_versionControl/Version2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Version2 : MonoBehaviour
+{
+    string name;
+    void Start()
+    {
+        name = "Dario";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
