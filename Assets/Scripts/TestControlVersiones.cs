192772f204276e5baf4b1478a1c86820547511f6
dario <0511dario@gmail.com>
2022-12-31 15:26:56 -0600

    Nuevo Script

---
+++ Assets/Scripts/TestControlVersiones.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestControlVersiones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola Mundo");
+    }
+
+    
+}
